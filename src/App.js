cffc54b45621511731ef2252fbaf44a724e85424
Welington da Silva <thebylito@gmail.com>
2019-09-09 14:02:25 -0400

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({ data: { organization }, loading }) => {
+      if (loading || !organization) {
+        return <div>Loading ...</div>;
+      }
+      return (
+        <RepositoryList repositories={organization.repositories} />
+      );
+    }}
+  </Query>
+);
+const RepositoryList = ({ repositories }) => (
+  <ul>
+    {repositories.edges.map(({ node }) => {
+      return (
+        <li key={node.id}>
+          <a href={node.url}>{node.name}</a>
+        </li>
+      );
+    })}
+  </ul>
+);
 export default App;
+
+// https://www.robinwieruch.de/react-apollo-client-example
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = new InMemoryCache();
+const API_BASE_URL = 'http://127.0.0.1:3333/graphql';
+const httpLink = new HttpLink({
+  uri: API_BASE_URL,
+  headers: {
+    // authorization: `Bearer ${
+    //   process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    // }`,
+  },
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
